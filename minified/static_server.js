be472b6b5da74e290a5720e22a0b0b2b9971c0d2
alu0100818208 <alu0100818208@ull.edu.es>
2016-03-31 00:52:13 +0100

    Borrada carpeta minified.
    Modificación gracias al issue @alu0100845235

--- minified/static_server.js
+++
@@ -1,1 +0,0 @@
-var static=require("node-static"),fileServer=new static.Server("./");console.log("Visit http://localhost:8080/test.html"),require("http").createServer(function(e,t){e.addListener("end",function(){fileServer.serve(e,t)}).resume()}).listen(8080);
